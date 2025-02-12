2b4ba8ce66bcad287d53b31bb05369af3752bbfc
Tianshuai <1321743979@qq.com>
2025-02-12 14:05:26 +0100

    added hcp terraform as remote backend

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,4 +1,12 @@
 terraform {
+  backend "remote" {
+    organization = "tianlu-terraform-org"
+
+    workspaces {
+      name = "news-search"
+    }
+  }
+
   required_providers {
     google = {
       source  = "hashicorp/google"
